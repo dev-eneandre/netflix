6dfd191a8330597b1c848cfd582c9862c6037c45
unknown <iameneandre@gmail.com>
2023-02-09 04:26:04 -0800

    row design

---
+++ src/Row.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+const Row = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      // return request.data.results;
+    }
+    fetchData().catch((err) => {
+      console.log(err.message);
+      console.log("something went wrong");
+    });
+  }, [fetchUrl]);
+
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h4>{title}</h4>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
